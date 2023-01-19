c24deb19f01c33d098fd05d63a717ec940fb560e
kelicaleb <kelicaleb7@gmail.com>
2023-01-19 14:14:41 +0300

    Completed Assignment

--- src/components/Tickler.js
+++ src/components/Tickler.js
@@ -1,11 +1,25 @@
 import React from "react";
+import {useState} from 'react'
 
 function Tickler() {
+  const [state, setUseState] = useState('')
   function tickle() {
     console.log("Teehee!");
   }
+  function submit(e){
+    e.preventDefault();
+    console.log(state)
+  }
 
-  return <button>Tickle me!</button>;
+  return(
+    <>
+    <button onClick={tickle}>Tickle me!</button>
+    <form onSubmit={submit}>
+      <input type="text" placeholder="username" value={state} onChange={(e)=> setUseState(e.target.value)}/>
+      <button>submit me </button>
+    </form>    
+    </>
+  )
 }
 
 export default Tickler;
